ede4dbdd7947b77ae8b879d15c07ebcf9704d4a7
Stephen Dolan <sdolan@janestreet.com>
2026-07-21 17:58:08 +0100

    Remove dead code from Dwarf_flags

--- backend/debug/dwarf/dwarf_flags/dwarf_flags.mli
+++ backend/debug/dwarf/dwarf_flags/dwarf_flags.mli
@@ -1,38 +1,3 @@
-type debug_thing =
-  | Debug_ocamldebug
-  | Debug_js_of_ocaml
-  | Debug_subprocs
-  | Debug_backtraces
-  | Debug_bounds_checking
-  | Debug_disable_bytecode_opt
-  | Debug_dwarf_cfi
-  | Debug_dwarf_loc
-  | Debug_dwarf_functions
-  | Debug_dwarf_scopes
-  | Debug_dwarf_vars
-  | Debug_dwarf_call_sites
-  | Debug_dwarf_cmm
-
-val debug_thing : debug_thing -> bool
-
-val set_debug_thing : debug_thing -> unit
-
-val clear_debug_thing : debug_thing -> unit
-
-val describe_debug_default : debug_thing -> string
-
-val describe_debug_default_negated : debug_thing -> string
-
-val use_g : unit -> unit
-
-val use_g0 : unit -> unit
-
-val use_g1 : unit -> unit
-
-val use_g2 : unit -> unit
-
-val use_g3 : unit -> unit
-
 val restrict_to_upstream_dwarf : bool ref
 
 val dwarf_max_function_complexity : int ref
